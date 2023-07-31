6993017016cd84583451a732fbac33c2ff2b8e0f
DESKTOP-41JMPNS\Gadget 360 <moeenuddin@xpertsovers.com>
2023-07-31 14:35:47 +0600

    updated

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,17 @@
- 
+const mobileMenu = document.querySelector(".mobile-menu");
+
+function showHideMenu() {
+  const abc = mobileMenu.getAttribute("class");
+  if (abc.includes("left-[-100%]")) {
+    mobileMenu.setAttribute(
+      "class",
+      "fixed left-0 top-0 w-2/5 h-screen z-50 bg-primary text-white p-3 mobile-menu"
+    );
+    mobileMenu.classList.add("dark");
+  } else {
+    mobileMenu.setAttribute(
+      "class",
+      "fixed left-[-100%] top-0 w-2/5 h-screen z-50 bg-primary text-white p-3 mobile-menu"
+    );
+  }
+}
