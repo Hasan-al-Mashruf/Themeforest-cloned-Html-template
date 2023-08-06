735069e3f910c7c31a20525021529dae229b4a96
Hasan al mashruf <you@example.commashruf125@gmail.com>
2023-08-06 11:42:23 +0600

    aos updated

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,17 +1,19 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 
 function showHideMenu() {
-  const abc = mobileMenu.getAttribute("class");
-  if (abc.includes("left-[-100%]")) {
+  const menuCheck = mobileMenu.getAttribute("class");
+  if (menuCheck.includes("left-[-100%]")) {
     mobileMenu.setAttribute(
       "class",
-      "fixed left-0 top-0 w-2/5 h-screen z-50 bg-primary text-white p-3 mobile-menu lg:hidden block"
+      "fixed left-0 top-0 md:w-2/5 w-3/5 h-screen z-50 bg-primary text-white p-3 mobile-menu lg:hidden block"
     );
     mobileMenu.classList.add("dark");
+    console.log("done");
   } else {
     mobileMenu.setAttribute(
       "class",
-      "fixed left-[-100%] top-0 w-2/5 h-screen z-50 bg-primary text-white p-3 mobile-menu lg:hidden block"
+      "fixed left-[-100%] top-0 md:w-2/5 w-3/5 h-screen z-50 bg-primary text-white p-3 mobile-menu lg:hidden block"
     );
+    console.log("not done");
   }
 }
